8cc133823d9c6890034e6ba02a79f5d7a9b718eb
juanpintom <juanpintom@gmail.com>
2016-02-12 02:32:13 +0100

    Added connection to Zozziarello
    Configurable IP at Start of py

--- SoulissToOH.py
+++ SoulissToOH.py
@@ -1,5 +1,12 @@
  # This Python file uses the following encoding: utf-8
-import os, sys
+import os, sys, requests
+
+ipaddress = '192.168.1.17'  # IP ADDRESS OF ZOZZARIELLO SERVER
+
+url = 'http://' + ipaddress + ':8080/structure?all'
+print (url)
+r = requests.get(url)
+r.encoding = "utf-8-sig"
 
 tfs = 'souliss.sitemap'
 f1 = open(tfs, 'w+')
@@ -8,7 +15,8 @@ tf = 'souliss.items'
 f2 = open(tf, 'w+') #f2 = open(tf, 'a+')
 
 import json
-data = {"id":[{"hlt":255,"slot":[{"val":0,"ddesc":"Luz Regulable","slo":0,"typ":"19"}],"ndesc":"Nodo O"},{"hlt":255,"slot":[{"val":26,"ddesc":"Sensor de Temperatura","slo":0,"typ":"52"},{"val":20,"ddesc":"Sensor de Humedad","slo":2,"typ":"53"},{"val":19,"ddesc":"Sensor de Temperatura","slo":4,"typ":"52"},{"val":50,"ddesc":"Sensor de Humedad","slo":6,"typ":"53"},{"val":179,"ddesc":"Sensor de luz","slo":8,"typ":"54"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":10,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":11,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":12,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":13,"typ":"11"},{"val":0,"ddesc":"Tira de LED RGB","slo":16,"typ":"16"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":20,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":21,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":22,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":23,"typ":"11"}],"ndesc":"Nodo I"},{"hlt":25,"slot":[{"val":18,"ddesc":"Sensor de Temperatura","slo":0,"typ":"52"},{"val":47,"ddesc":"Sensor de Humedad","slo":2,"typ":"53"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":4,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":5,"typ":"11"},{"val":0,"ddesc":"Luz Regulable","slo":6,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":8,"typ":"19"},{"val":154,"ddesc":"Sensor de luz","slo":10,"typ":"54"},{"val":240,"ddesc":"ON\/OFF Salida con modo AUT","slo":12,"typ":"12"},{"val":1.4296875,"ddesc":"Sensor de Corriente","slo":13,"typ":"56"},{"val":329,"ddesc":"Sensor de Potencia","slo":15,"typ":"57"}],"ndesc":"Nodo II"},{"hlt":25,"slot":[],"ndesc":"Nodo III"},{"hlt":255,"slot":[{"val":20,"ddesc":"Sensor de Temperatura","slo":0,"typ":"52"},{"val":37,"ddesc":"Sensor de Humedad","slo":2,"typ":"53"},{"val":0,"ddesc":"Luz Regulable","slo":4,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":6,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":8,"typ":"19"},{"val":0,"ddesc":"Sensor de luz","slo":10,"typ":"54"}],"ndesc":"Nodo IV"},{"hlt":1,"slot":[{"val":16.203125,"ddesc":"Sensor de Temperatura","slo":0,"typ":"52"},{"val":60.3125,"ddesc":"Sensor de Humedad","slo":2,"typ":"53"},{"val":0,"ddesc":"Luz Regulable","slo":4,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":6,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":8,"typ":"19"},{"val":0,"ddesc":"Sensor de luz","slo":10,"typ":"54"},{"val":15.75,"ddesc":"Sensor de Temperatura","slo":12,"typ":"52"}],"ndesc":"Nodo V"},{"hlt":255,"slot":[{"val":22,"ddesc":"Sensor de Temperatura","slo":0,"typ":"52"},{"val":37,"ddesc":"Sensor de Humedad","slo":2,"typ":"53"},{"val":0,"ddesc":"Luz Regulable","slo":4,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":6,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":8,"typ":"19"},{"val":40,"ddesc":"Sensor de Temperatura","slo":10,"typ":"52"},{"val":124,"ddesc":"Entrada analÃ³gica","slo":12,"typ":"51"},{"val":0,"ddesc":"Entrada analÃ³gica","slo":14,"typ":"51"},{"val":0,"ddesc":"Entrada analÃ³gica","slo":16,"typ":"51"},{"val":0,"ddesc":"Entrada analÃ³gica","slo":18,"typ":"51"}],"ndesc":"Nodo VI"}]} 
+#data = {"id":[{"hlt":255,"slot":[{"val":0,"ddesc":"Luz Regulable","slo":0,"typ":"19"}],"ndesc":"Nodo O"},{"hlt":255,"slot":[{"val":26,"ddesc":"Sensor de Temperatura","slo":0,"typ":"52"},{"val":20,"ddesc":"Sensor de Humedad","slo":2,"typ":"53"},{"val":19,"ddesc":"Sensor de Temperatura","slo":4,"typ":"52"},{"val":50,"ddesc":"Sensor de Humedad","slo":6,"typ":"53"},{"val":179,"ddesc":"Sensor de luz","slo":8,"typ":"54"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":10,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":11,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":12,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":13,"typ":"11"},{"val":0,"ddesc":"Tira de LED RGB","slo":16,"typ":"16"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":20,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":21,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":22,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":23,"typ":"11"}],"ndesc":"Nodo I"},{"hlt":25,"slot":[{"val":18,"ddesc":"Sensor de Temperatura","slo":0,"typ":"52"},{"val":47,"ddesc":"Sensor de Humedad","slo":2,"typ":"53"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":4,"typ":"11"},{"val":0,"ddesc":"ON\/OFF Salida digital con timer","slo":5,"typ":"11"},{"val":0,"ddesc":"Luz Regulable","slo":6,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":8,"typ":"19"},{"val":154,"ddesc":"Sensor de luz","slo":10,"typ":"54"},{"val":240,"ddesc":"ON\/OFF Salida con modo AUT","slo":12,"typ":"12"},{"val":1.4296875,"ddesc":"Sensor de Corriente","slo":13,"typ":"56"},{"val":329,"ddesc":"Sensor de Potencia","slo":15,"typ":"57"}],"ndesc":"Nodo II"},{"hlt":25,"slot":[],"ndesc":"Nodo III"},{"hlt":255,"slot":[{"val":20,"ddesc":"Sensor de Temperatura","slo":0,"typ":"52"},{"val":37,"ddesc":"Sensor de Humedad","slo":2,"typ":"53"},{"val":0,"ddesc":"Luz Regulable","slo":4,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":6,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":8,"typ":"19"},{"val":0,"ddesc":"Sensor de luz","slo":10,"typ":"54"}],"ndesc":"Nodo IV"},{"hlt":1,"slot":[{"val":16.203125,"ddesc":"Sensor de Temperatura","slo":0,"typ":"52"},{"val":60.3125,"ddesc":"Sensor de Humedad","slo":2,"typ":"53"},{"val":0,"ddesc":"Luz Regulable","slo":4,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":6,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":8,"typ":"19"},{"val":0,"ddesc":"Sensor de luz","slo":10,"typ":"54"},{"val":15.75,"ddesc":"Sensor de Temperatura","slo":12,"typ":"52"}],"ndesc":"Nodo V"},{"hlt":255,"slot":[{"val":22,"ddesc":"Sensor de Temperatura","slo":0,"typ":"52"},{"val":37,"ddesc":"Sensor de Humedad","slo":2,"typ":"53"},{"val":0,"ddesc":"Luz Regulable","slo":4,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":6,"typ":"19"},{"val":0,"ddesc":"Luz Regulable","slo":8,"typ":"19"},{"val":40,"ddesc":"Sensor de Temperatura","slo":10,"typ":"52"},{"val":124,"ddesc":"Entrada analógica","slo":12,"typ":"51"},{"val":0,"ddesc":"Entrada analógica","slo":14,"typ":"51"},{"val":0,"ddesc":"Entrada analógica","slo":16,"typ":"51"},{"val":0,"ddesc":"Entrada analógica","slo":18,"typ":"51"}],"ndesc":"Nodo VI"}]} 
+data = r.json()
 #encoded
 data_string = json.dumps(data)
  
@@ -35,7 +43,7 @@ f2.write('Group weather "Metereologia" <weather> (all)'  + '\n')
 f2.write('Group temperature "Temperatura" <temperature> (weather)'  + '\n')
 f2.write('Group humidity "Humedad" <humidity> (weather)'  + '\n')
 f2.write('Group lux "Luz Ambiental" <lux> (weather)'  + '\n')
-f2.write('Group:Number:AVG() Temperaturas_Medias "Temperaturas Medias [%.1f Â°C]" <temperature> (weather)'  + '\n')
+f2.write('Group:Number:AVG() avg_temperatures "Temperaturas Medias [%.1f C]" <temperature> (weather)'  + '\n')
 f2.write('Group light "Luces" <light> (all)'  + '\n')
 #QUEDAN MAS GRUPOS POR PONER
 
@@ -59,7 +67,7 @@ for i in decoded["id"]:
 
     site = 'Text item=timestamp_node' + str(nod)
     f1.write(site.encode('latin1') + '\n')
-    line = 'String timestamp_node' + str(nod) + ' "Timestamp ' + ndesc + ' [%1$td.%1$tm.%1$tY %1$tk:%1$tM:%1$tS]" <keyring> (node' + str(nod) + ', diagnostic) {souliss="D99' + ':' + str(nod) + ':999"}' 
+    line = 'String timestamp_node' + str(nod) + ' "Timestamp ' + ndesc + ' [%1$td.%1$tm.%1$tY %1$tk:%1$tM:%1$tS]" <clock> (node' + str(nod) + ', diagnostic) {souliss="D99' + ':' + str(nod) + ':999"}' 
     f2.write(line.encode('latin1') + '\n')
 
     for j in decoded["id"][nod]["slot"]:
@@ -108,10 +116,10 @@ for i in decoded["id"]:
             line = 'Number node' + str(nod) + '_slot' + slo + ' "' + ddesc + ' [%.1f lux]" <lux> (node' + str(nod) + ', lux) {souliss="T' + str(typ) + ':' + str(nod) + ':' + slo + '"}' 
         elif (typ == 56):
             site = 'Text item=node' + str(nod) + '_slot' + slo
-            line = 'Number node' + str(nod) + '_slot' + slo + ' "' + ddesc + ' [%.1f A]" <current> (node' + str(nod) + ', current) {souliss="T' + str(typ) + ':' + str(nod) + ':' + slo + '"}' 
+            line = 'Number node' + str(nod) + '_slot' + slo + ' "' + ddesc + ' [%.1f A]" <energy> (node' + str(nod) + ', current) {souliss="T' + str(typ) + ':' + str(nod) + ':' + slo + '"}' 
         elif (typ == 57):
             site = 'Text item=node' + str(nod) + '_slot' + slo
-            line = 'Number node' + str(nod) + '_slot' + slo + ' "' + ddesc + ' [%.1f W]" <watt> (node' + str(nod) + ', watt) {souliss="T' + str(typ) + ':' + str(nod) + ':' + slo + '"}' 
+            line = 'Number node' + str(nod) + '_slot' + slo + ' "' + ddesc + ' [%.1f W]" <energy> (node' + str(nod) + ', watt) {souliss="T' + str(typ) + ':' + str(nod) + ':' + slo + '"}' 
         elif (typ == 58):
             site = 'Text item=node' + str(nod) + '_slot' + slo
             line = 'Number node' + str(nod) + '_slot' + slo + ' "' + ddesc + ' [%.1f mb]" <pressure> (node' + str(nod) + ', pressure) {souliss="T' + str(typ) + ':' + str(nod) + ':' + slo + '"}' 
@@ -127,7 +135,10 @@ for i in decoded["id"]:
     nod = nod + 1
     slot = 0
 nod = 0 
-footer = '}' + '\n' + '}' + '\n' + '}'
+f1.write('}' + '\n')  #END OF LAST NODE
+f1.write('Text item=avg_temperatures' + '\n')
+footer = '}' + '\n' + '}'
 f1.write(footer)
 f1.close()  
 f2.close()
+
